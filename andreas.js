4a46371ce65f949246171954b24c775cae34c5e2
Andreas Åkerlöf <andreas@akerlof.se>
2019-08-22 10:06:28 +0200

    Revert "Coca cola zero bild + funktion runt innehåll i js-fil"
    This reverts commit 106d5c236173b2363fe0b907bfa7dc6bb2dd99e1.

--- andreas.js
+++ andreas.js
@@ -1,45 +1,41 @@
-(()=> {
+// Selektorer
+const contact = document.querySelector('#contact');
+const contactImg = document.querySelector('.contact-img');
 
-  // Selektorer
-  const contact = document.querySelector('#contact');
-  const contactImg = document.querySelector('.contact-img');
-  
-  // Funktion som animerar kontaktdelen
-  function animateContact() {
-    if (isElementInViewport(contact)) {
-      contactImg.classList.add('contact-img-active')  // Bild
-      anime({
-        targets: '.contact-form > *',
-        translateX: '-50vw',
-        delay: (el,i) => i*10,
-        duration: 2000,
-        easing: 'easeOutElastic'
-      });
-    }
-    else {
-      contactImg.classList.remove('contact-img-active');
-      anime({
-        targets: '.contact-form > *',
-        translateX: '50vw',
-      });
-    } 
+// Funktion som animerar kontaktdelen
+function animateContact() {
+  if (isElementInViewport(contact)) {
+    contactImg.classList.add('contact-img-active')  // Bild
+    anime({
+      targets: '.contact-form > *',
+      translateX: '-50vw',
+      delay: (el,i) => i*10,
+      duration: 2000,
+      easing: 'easeOutElastic'
+    });
   }
-  
-  // Eventlisteners som triggar kontaktdelens animationer
-  addEventListener('DOMContentLoaded', animateContact, false); 
-  addEventListener('load', animateContact, false);
-  addEventListener('scroll', animateContact, false);
-  
-  // Funktion för att kontrollera om ett element är i viewporten
-  function isElementInViewport (el) {
-    var rect = el.getBoundingClientRect();
-  
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
-    );
-  }
+  else {
+    contactImg.classList.remove('contact-img-active');
+    anime({
+      targets: '.contact-form > *',
+      translateX: '50vw',
+    });
+  } 
+}
 
-})();
+// Eventlisteners som triggar kontaktdelens animationer
+addEventListener('DOMContentLoaded', animateContact, false); 
+addEventListener('load', animateContact, false);
+addEventListener('scroll', animateContact, false);
+
+// Funktion för att kontrollera om ett element är i viewporten
+function isElementInViewport (el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+  );
+}
